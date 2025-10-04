2be966853bb701e2266ecad448c3fac8c1aadd72
oscar sanchez <00oscarsanchez00@gmail.com>
2025-10-04 13:38:42 -0500

    got simple assignment done on lab9

--- week1/lab9.py
+++ week1/lab9.py
@@ -7,17 +7,28 @@ import gradio as gr
 
 load_dotenv(override=True)
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+""" CHAT LLM """
 MODEL = "gpt-4.1-mini"
 openai = OpenAI()
 
+""" SYSTEM MESSAGE """
 system_msg = "You are a helpful assistant for an Airline called FlightAI. "
 system_msg += "Give short, witty, snarky answers, no more than 1 sentence."
-system_msg += "If relevant, use the get_ticket_price tool to get the price of a ticket to a given city."
+# system_msg += "If relevant, use the get_ticket_price tool to get the price of a ticket to a given city."
 
-""" CHAT LLM """
+""" TOOLS """
+ticket_prices = {"london": "$799", "paris": "$899", "tokyo": "$1400", "sydney": "$2999"}
 
 
-def chat(message, history):
+def get_ticket_price(city):
+    return f"The price of a ticket to {city.title()} is $799."
+
+
+""" SIMPLE CHAT LLM FUNCTION TO USE IN THE GRADIO UI """
+
+
+def simple_chat(message, history):
     messages = (
         [{"role": "system", "content": system_msg}]
         + history
@@ -44,3 +55,48 @@ def artist_agent(city):
     image_base64 = image_response.data[0].b64_json
     image_data = base64.b64decode(image_base64)
     return Image.open(BytesIO(image_data))
+
+
+""" CHAT LLM WITH IMAGE LLM """
+
+system_msg += "If relevant, the price of a ticket to London is $799."
+
+
+def mixed_chat(history):
+    message = history[-1]["content"]
+    messages = [{"role": "system", "content": system_msg}] + history
+
+    city = None
+    for dest in ticket_prices.keys():
+        if dest in message.lower():
+            city = dest
+            break
+    if city:
+        reply = get_ticket_price(city)
+        image = artist_agent(city)
+    else:
+        response = openai.chat.completions.create(model=MODEL, messages=messages)
+        reply = response.choices[0].message.content
+        image = None
+
+    history += [{"role": "assistant", "content": reply}]
+    return history, image
+
+
+""" GRADIO UI """
+with gr.Blocks() as ui:
+    with gr.Row():
+        chatbot = gr.Chatbot(height=400, type="messages")
+        image_output = gr.Image(height=400)
+    with gr.Row():
+        entry = gr.Textbox(label="Chat with our AI Assistant:")
+
+    def do_entry(message, history):
+        history += [{"role": "user", "content": message}]
+        return "", history
+
+    entry.submit(do_entry, inputs=[entry, chatbot], outputs=[entry, chatbot]).then(
+        mixed_chat, inputs=chatbot, outputs=[chatbot, image_output]
+    )
+
+ui.launch(inbrowser=True)
